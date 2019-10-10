3107fad883d1afdbf226cfcf6e19fab934e7138e
henry0822 <henrylin.0822@gmail.com>
2019-10-10 12:24:04 +0800

    Empty Unity Project Commit

---
+++ StarvingShark/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 2.0f;
+    public  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.)
+    }
+}
