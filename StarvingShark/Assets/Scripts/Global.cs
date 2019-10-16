e26c87980eec41a4f68c0eb2392426999b077e98
fayian1415 <ianleetaiwan@outlook.com>
2019-10-16 21:11:58 +0800

    Create Depth Warning System Death under certain depth

--- StarvingShark/Assets/Scripts/Global.cs
+++ StarvingShark/Assets/Scripts/Global.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public enum GameStatus { RUNNING, PAUSE };
 public static class Global {
+    public static readonly int meterPerUnit = 100; //1 (unityunit) == 100(meter)
     public static GameStatus gameStatus = GameStatus.RUNNING;
     public static void GameOver() {
         gameStatus = GameStatus.PAUSE;
