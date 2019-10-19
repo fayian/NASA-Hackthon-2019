70a79c996af2bdb082561400d8dc28ad17afd544
henry0822 <henrylin.0822@gmail.com>
2019-10-19 17:34:44 +0800

    Menu

--- TurtleSimulator/Assets/Scripts/Menu.cs
+++ TurtleSimulator/Assets/Scripts/Menu.cs
@@ -5,15 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
