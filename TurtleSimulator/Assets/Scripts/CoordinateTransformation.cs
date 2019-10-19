5949d02075273b916de5a9ad6736483bd0a7021f
fayian1415 <ianleetaiwan@outlook.com>
2019-10-19 15:36:42 +0800

    Finished Geographic Coordnate System

--- TurtleSimulator/Assets/Scripts/CoordinateTransformation.cs
+++ TurtleSimulator/Assets/Scripts/CoordinateTransformation.cs
@@ -3,19 +3,39 @@ using UnityEngine;
 
 public class CoordinateTransformation : MonoBehaviour
 {
-    public Transform player;
-    public Text coordinate;
-    private float coordinateRate = 0.01f;
-    
+    private Text coordText;
+
+    private const float MIN_LATITIUDE = 10; //N
+    private const float MAX_LATITIUDE = 40; //N
+    private const float MIN_LONGTITUDE = 150; //E
+    private const float MAX_LONGTITUDE = 220; /*-140W*/
+
     private float longtitude;
     private float latitude;
     private float depth;
 
+    private string longtitudeText;
+    public void UVMapping(float u, float v) {
+        longtitude = (MAX_LONGTITUDE - MIN_LONGTITUDE) * u + MIN_LONGTITUDE;
+        latitude = (MAX_LATITIUDE - MIN_LATITIUDE) * v + MIN_LATITIUDE;
+    }
+
+    void Awake() {
+        Global.player.GetComponent<PlayerMovement>().coords = this;
+        coordText = GetComponent<Text>();
+    }
     void Update()
     {
-        longtitude = player.position.x * coordinateRate+150.0f;
-        latitude = player.position.z * coordinateRate+23.5f;
-        depth = -player.position.y * Global.METER_PER_UNIT;
-        coordinate.text = "Longtitude:" + longtitude.ToString("F2") + "E\r\n" + "Latitude:" + latitude.ToString("F2") + "N\r\n" + "Depth:" + depth.ToString("F2") + "m";
+        depth = -Global.player.transform.position.y * Global.METER_PER_UNIT;
+
+        if (longtitude < 180)
+            longtitudeText = "Longtitude: " + longtitude.ToString("F2") + "°E\r\n";
+        else if(longtitude > 180)
+            longtitudeText = "Longtitude: " + (360 - longtitude).ToString("F2") + "°W\r\n";
+        else
+            longtitudeText = "Longtitude: 180.00°\r\n";
+
+
+        coordText.text = longtitudeText + "Latitude:" + latitude.ToString("F2") + "N\r\n" + "Depth:" + depth.ToString("F2") + "m";
     }
 }
